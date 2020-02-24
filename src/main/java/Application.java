58ca596df5c3f1d757cac2cd3566be62a63f4ef2
sylwiapytko <sylwia.pytko@gmail.com>
2020-02-24 19:58:10 +0100

    AppTwitterControler

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,39 +9,8 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) throws TwitterException, InterruptedException, IOException {
 
-        System.out.println("Hello World");
-
-        Twitter twitter = TwitterFactory.getSingleton();
-        //String message="Hello to Java";
-        //twitter.updateStatus(message);
-
-
-        TwitterControler twitterControler = new TwitterControler();
-
-        //String userName = "eitimagresea";
-        //String userName = "chomik_slawomir";
-        //String userName = "oko_press";
-        String userName = "hildatheseries";
-        Long tweetId = 1228383913460092929L;
-        //twitterControler.printUserTimeline(twitter, userName, 1, 2);
-        //twitterControler.getUserTimelineList(twitter, userName);
-        //System.out.println(twitter.showUser(userName).getStatusesCount());
-        //twitterControler.printURLfromMyTweets(twitter);
-        //twitterControler.printFavourites(twitter);
-        //twitterControler.printFollowersList(twitter,userName);
-        //twitterControler.printFollowersIDList(twitter,userName);
-        //twitterControler.countFollowers(twitter,userName);
-        //twitterControler.printRetweets(twitter,tweetId);
-       // twitterControler.printRetweeterIDList(twitter,tweetId);
-        //twitterControler.countRetweets(twitter,tweetId);
-        //twitterControler.countRetweets2(twitter,tweetId);
-
-        //twitterControler.writeUserTimelineToFile(twitter,userName);
-
-        twitterControler.printUser(twitter,userName);
-
-
-        System.out.println("End");
+        AppTwitterControler appTwitterControler = new AppTwitterControler();
+        appTwitterControler.testTwitterController();
 
     }
 }
